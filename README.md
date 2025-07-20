616729690624d7c9f71a6b15e69273b6b47c368d
buulam <buulam@gmail.com>
2025-07-19 17:13:00 -0700

    Cleaned some things up

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # tubetelemetry
 
 ```
-pip install fastapi uvicorn httpx dotenv google-auth google-auth-oauthlib
+pip install fastapi uvicorn httpx dotenv google-auth google-auth-oauthlib Requests
 ```
 
 Create a .env file
@@ -9,5 +9,5 @@ Create a .env file
 ```
 VIEWS=https://youtubeanalytics.googleapis.com/v2/reports?endDate=2025-07-31&filters=video%3D%3DNG2_AE2_zOg&ids=channel%3D%3DMINE&metrics=views%2Ccomments%2Clikes%2Cdislikes%2CestimatedMinutesWatched%2CaverageViewDuration&startDate=2023-09-01
 SCOPES=https://www.googleapis.com/auth/yt-analytics.readonly
-VIDEO=
+VIDEO=this is where you put the video ID (appened on video URL's after v=)
 ```
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 import httpx, os, google.auth.transport.requests
@@ -8,15 +7,9 @@ import httpx, os, google.auth.transport.requests
 load_dotenv()
 app = FastAPI()
 SCOPES = os.getenv("SCOPES")
-flow = InstalledAppFlow.from_client_secrets_file(
-    '.client_secret_661009110553-nfopgtp2jbbgssei7upjh7k7unltro4p.apps.googleusercontent.com.json', SCOPES)
-creds = flow.run_local_server(port=0)
+CLIENT_SECRET_FILE = os.getenv("CLIENT_SECRET_FILE")
 VIEWS = os.getenv("VIEWS")
 VIDEO = os.getenv("VIDEO")
-
-# Save the token for later use
-with open('.token.json', 'w') as token:
-    token.write(creds.to_json())
 
 @app.get("/")
 def read_root():
@@ -28,7 +21,15 @@ async def read_api():
         response = await client.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
         data = response.json()
         return {"what": data}
-    
+
+@app.get("/new-token")
+async def new_token():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+    creds = flow.run_local_server(port=0)
+    with open('.token.json', 'w') as token:
+        token.write(creds.to_json())
+    return {"token": creds.token}
+
 @app.get("/google-profile")
 async def get_google_profile():
     creds = Credentials.from_authorized_user_file('.token.json')
